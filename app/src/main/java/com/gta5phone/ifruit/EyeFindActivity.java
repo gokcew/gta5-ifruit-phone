232db96c0b095f76e789e4a4f155ec5278505387
gokcew <gokcemehmet1111@gmail.com>
2026-08-26 17:43:54 +0300

    upload app/src/main/java/com/gta5phone/ifruit/EyeFindActivity.java

---
+++ app/src/main/java/com/gta5phone/ifruit/EyeFindActivity.java
@@ -0,0 +1,25 @@
+package com.gta5phone.ifruit;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EyeFindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.activity_eyefind);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+}
